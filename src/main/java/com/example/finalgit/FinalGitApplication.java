c7d7ec358bf74a61ee8a104cc8d11f22fca99601
Mikhail Sokovykh <misha.sokovih@gmail.com>
2022-11-26 16:17:50 +0300

    Initial commit

---
+++ src/main/java/com/example/finalgit/FinalGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.finalgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalGitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/finalgit/FinalGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finalgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
